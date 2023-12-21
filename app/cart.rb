27772d557588d67f66311c5da127d1646d37bf0c
Krzysztof Marszalek <krzys.marszalek@gmail.com>
2023-12-21 08:51:56 +0100

    Discount rules

--- app/cart.rb
+++ app/cart.rb
@@ -1,21 +1,48 @@
 class Cart
-  attr_reader :items
+  attr_reader :items, :total_price
 
-  def initialize
+  def initialize(discount_rules = {})
+    @discount = 0
+    @discount_rules = discount_rules
     @items = []
   end
 
   def add_item(product, quantity = 1)
-    existing_item = @items.find { |item| item.product.sku == product.sku }
+    existing_item = items.find { |item| item.product.sku == product.sku }
 
     if existing_item
       existing_item.quantity += quantity
     else
-      @items << CartItem.new(product, quantity)
+      items << CartItem.new(product, quantity)
     end
   end
 
   def total_price
-    @items.sum(&:total_price)
+    (items.sum(&:total_price) - @discount).round(2)
+  end
+
+  def calculate_discount
+    items.each do |item|
+      rule = @discount_rules[item.product.sku]
+      return unless rule
+
+      case rule[:name]
+      when 'second-item-free'
+        free_items_count = (item.quantity / 2).floor
+        @discount += item.product.price * free_items_count
+        break
+
+      when 'total-percentage-discount'
+        if item.quantity >= rule[:quantity]
+          @discount += item.total_price * rule[:value] / 100
+        end
+        break
+
+      when 'single-price-discount'
+        if item.quantity >= rule[:quantity]
+          @discount += (item.product.price - rule[:value]) * item.quantity
+        end
+      end
+    end
   end
 end
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -6,17 +6,18 @@ require_relative '../app/cart_item'
 RSpec.describe Cart do
   let(:product1) { Product.new('abc123', 'Banana', 33.22) }
   let(:product2) { Product.new('def456', 'Apple', 10.50) }
+  let(:product3) { Product.new('ghi789', 'Orange', 20.22) }
 
   describe '#add_item' do
     let(:cart) { Cart.new }
 
     before do
-      subject.add_item(product1)
+      cart.add_item(product1)
     end
 
     it 'adds item to cart' do
-      expect(subject.items.size).to eq(1)
-      expect(subject.items.first.product).to eq(product1)
+      expect(cart.items.size).to eq(1)
+      expect(cart.items.first.product).to eq(product1)
     end
   end
 
@@ -24,13 +25,66 @@ RSpec.describe Cart do
     let(:cart) { Cart.new }
 
     before do
-      subject.add_item(product1)
-      subject.add_item(product1)
-      subject.add_item(product2)
+      cart.add_item(product1)
+      cart.add_item(product1)
+      cart.add_item(product2)
     end
 
     it 'calculates total price' do
-      expect(subject.total_price).to eq(76.94)
+      expect(cart.items.first.quantity).to eq(2)
+      expect(cart.total_price).to eq(76.94)
+    end
+  end
+
+  describe '#apply_discount_rules' do
+  # config format: { sku: { name: 'rule-name', quantity: 3, value: 10 } }
+    let(:discount_rules) {
+      {
+        "abc123" => { name: "second-item-free" },
+        "def456" => { name: "total-percentage-discount", quantity: 3, value: 10 },
+        "ghi789" => { name: "single-price-discount", quantity: 3, value: 15.50 }
+      }
+    }
+
+    let(:cart) { Cart.new(discount_rules) }
+
+    context 'buy one get one free' do
+      before do
+        cart.add_item(product1)
+        cart.add_item(product1)
+        cart.add_item(product1)
+        cart.calculate_discount
+      end
+
+      it 'calculates total price with discount' do
+        expect(cart.total_price).to eq(product1.price * 2)
+      end
+    end
+
+    context 'total percentage discount' do
+      before do
+        cart.add_item(product2)
+        cart.add_item(product2)
+        cart.add_item(product2)
+        cart.calculate_discount
+      end
+
+      it 'calculates total price with discount' do
+        expect(cart.total_price).to eq(product2.price * 3 * 0.9)
+      end
+    end
+
+    context 'single price discount' do
+      before do
+        cart.add_item(product3)
+        cart.add_item(product3)
+        cart.add_item(product3)
+        cart.calculate_discount
+      end
+
+      it 'calculates total price with discount' do
+        expect(cart.total_price).to eq(15.50 * 3)
+      end
     end
   end
 end
